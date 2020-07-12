f70953ec20a9c7ef927663364ea1b79dca57159a
Mateusz Belka <belka.mateusz.bm@gmail.com>
2020-07-12 12:46:36 +0200

    placeholder custom help and reorganize stuff

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,13 @@
-from discord.ext import commands
-from dotenv import load_dotenv, find_dotenv
+import time
+
 import boto3
 import os
 import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv, find_dotenv
 
-from AWS import factorio_server, util
+from aws import factorio_server, util
 from util import messages
 
 load_dotenv(find_dotenv())
@@ -12,6 +15,7 @@ TOKEN = os.environ.get("TOKEN")
 INSTANCE_ID = os.environ.get("INSTANCE_ID")
 
 client = commands.Bot(command_prefix='!')  # When typing bot commands, always start with '!'
+client.remove_command('help')
 
 ec2 = boto3.resource('ec2')
 instance = ec2.Instance(INSTANCE_ID)
@@ -24,16 +28,30 @@ async def on_ready():
     print('ID: {}'.format(client.user.id))
     print('Factorio server status: {}!'.format(util.get_state()))
     print('------------')
+    factorio_channel = await messages.clear_factorio_text_channel(client)
+    await messages.factorio_welcome_message(factorio_channel)
+    await factorio_channel.send(embed=messages.help_embed())
 
 
 @client.event
 async def on_member_join(member):
     print(f'{member} joined the SHR1MP Clan!')
+    # print jest do terminala a nie do discorda
+    # ciebie interesuje chyba context.send(string)
 
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} left the SHR1MP Clan...')
+    # print jest do terminala a nie do discorda
+    # ciebie interesuje chyba context.send(string)
+
+
+@client.command()
+async def help(context):
+    author = context.message.author
+    await author.send(embed=messages.help_embed())
+    await messages.clear(context, 1)
 
 
 @client.command()
@@ -60,7 +78,7 @@ async def factorio_on(context):
     if util.get_state() == "stopped":
         await factorio_server.turn_on_instance(context, instance)
     elif util.get_state() == "running":
-        await context.channel.send("Factorio server is already on!")
+        await context.send("Factorio server is already **ONLINE**!")
     else:
         await messages.print_error(context, "Server is either being turned off or on right now. Wait")
 
@@ -70,19 +88,26 @@ async def factorio_off(context):
     if util.get_state() == "running":
         await factorio_server.turn_off_instance(context, instance)
     elif util.get_state() == "stopped":
-        await context.channel.send("Factorio server is already off!")
+        await context.send("Factorio server is already **OFFLINE**!")
     else:
         await messages.print_error(context, "Server is either being turned off or on right now. Wait")
 
 
 @client.command()
 async def factorio_status(context):
-    await util.send_state_message(context)
+    channel = context.channel
+    await messages.purge(channel)
+    await util.send_state_message(channel, "Factorio")
 
 
 @client.command(pass_context=True)
 async def clear(context, number: int):
-    await messages.clear(context, number)
+    await messages.clear(context, number + 1)
+
+
+@client.command()
+async def nuke(context):
+    return await messages.reset_channel(context, discord)
 
 
 client.run(TOKEN)
