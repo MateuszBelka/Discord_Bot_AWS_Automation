6b66b552a00b61f83dda99fcc6884a4496b2034c
Emil <uvast8@gmail.com>
2020-07-12 21:06:43 +0200

    Added unban command

--- bot/bot.py
+++ bot/bot.py
@@ -119,5 +119,19 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
+    await ctx.send(f'Zbanowano {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Odbanowano {user.mention}')
+            return
 
 client.run(TOKEN)
