c8337fe9c3e30d2d12af908a5efe60fda37d7693
Emil <uvast8@gmail.com>
2020-07-13 17:55:02 +0200

    Added id to privilage

--- bot/util/privileges.py
+++ bot/util/privileges.py
@@ -2,5 +2,5 @@ import discord
 from discord.ext import commands
 
 def nuke_priv(ctx):
-    if (ctx.author.id == 105056143155441664):
+    if (ctx.author.id == 261991937949696000 or ctx.author.id == 105056143155441664):
         return True
