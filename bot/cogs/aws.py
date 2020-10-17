d939b6a350b19e997634f92cbab307b3bf3c43ab
Mateusz Belka <belka.mateusz.bm@gmail.com>
2020-10-17 07:41:05 +0200

    minecraft server auto turn off

--- bot/cogs/aws.py
+++ bot/cogs/aws.py
@@ -1,7 +1,7 @@
 # Author:   Mateusz Belka
 # Created:  13-Jul-2020
 import os
-import boto3
+import time
 from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
 
@@ -20,16 +20,16 @@ class Aws(commands.Cog):
                         "bot-minecraft-vanilla": "Minecraft",
                         "bot-terraria": "Terraria"}
     channel_instanceId_map = {"bot-factorio": factorio_instance_id,
-                            "bot-minecraft-vanilla": minecraft_vanilla_instance_id,
-                            "bot-terraria": terraria_instance_id}
+                              "bot-minecraft-vanilla": minecraft_vanilla_instance_id,
+                              "bot-terraria": terraria_instance_id}
 
     def __init__(self, client):
         self.client = client
 
-    async def is_proper_server_cmd_length(self, ctx, *cmds):
+    async def is_proper_server_cmd_length(self, channel, *cmds):
         if len(cmds) != 1:
             print("Wrong number of arguments")
-            await messages.perror(ctx, "Wrong number of arguments")
+            await messages.perror(channel, "Wrong number of arguments")
             return False
         print("Correct number of arguments")
         return True
@@ -39,14 +39,14 @@ class Aws(commands.Cog):
         if channel_name in self.supported_channels:
             return True
         print("Wrong channel")
-        await messages.perror(ctx, "Wrong channel")
+        await messages.perror(ctx.channel, "Wrong channel")
         return False
 
     # Commands
     @commands.command(brief="{on, start}, {off, stop}, reboot, status")
     async def server(self, ctx, *cmds):
         print("\nProcessing server command")
-        if not await self.is_proper_server_cmd_length(ctx, *cmds):
+        if not await self.is_proper_server_cmd_length(ctx.channel, *cmds):
             return
         if not await self.is_proper_discord_channel(ctx):
             return
@@ -62,33 +62,36 @@ class Aws(commands.Cog):
             await self.server_reboot(ctx)
         elif 'status' in cmds:
             print("Checking server status!")
-            await messages.aws_server_status_message_known_channel(ctx.channel)
+            await messages.aws_server_status_message(ctx.channel)
         else:
-            await messages.perror(ctx, "Try: $server + {on, start, off, stop, reboot, status}")
+            await messages.perror(ctx.channel, "Try: $server + {on, start, off, stop, reboot, status}")
 
     async def server_start(self, ctx):
         if aws.get_state(ctx.channel) == "stopped":
-            await aws_instance_state.turn_on_instance(ctx, self.channel_instanceId_map[ctx.channel.name])
+            await aws_instance_state.turn_on_instance(ctx.channel, self.channel_instanceId_map[ctx.channel.name])
         elif aws.get_state(ctx.channel) == "running":
-            await ctx.send("The {} server is already **ONLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
+            await ctx.send(
+                "The {} server is already **ONLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
         else:
-            await messages.perror(ctx, "Server is either being turned off or on right now. Wait")
+            await messages.perror(ctx.channel, "Server is either being turned off or on right now. Wait")
 
     async def server_stop(self, ctx):
         if aws.get_state(ctx.channel) == "running":
-            await aws_instance_state.turn_off_instance(ctx, self.channel_instanceId_map[ctx.channel.name])
+            await aws_instance_state.turn_off_instance(ctx.channel, self.channel_instanceId_map[ctx.channel.name])
         elif aws.get_state(ctx.channel) == "stopped":
-            await ctx.send("The {} server is already **OFFLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
+            await ctx.send(
+                "The {} server is already **OFFLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
         else:
-            await messages.perror(ctx, "Server is either being turned off or on right now. Wait")
+            await messages.perror(ctx.channel, "Server is either being turned off or on right now. Wait")
 
     async def server_reboot(self, ctx):
         if aws.get_state(ctx.channel) == "running":
-            await aws_instance_state.reboot_instance(ctx, self.channel_instanceId_map[ctx.channel.name])
+            await aws_instance_state.reboot_instance(ctx.channel, self.channel_instanceId_map[ctx.channel.name])
         elif aws.get_state(ctx.channel) == "stopped":
-            await ctx.send("The {} server is already **OFFLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
+            await ctx.send(
+                "The {} server is already **OFFLINE**!".format(self.channel_game_map[ctx.channel.name].lower()))
         else:
-            await messages.perror(ctx, "Server is either being turned off or on right now. Wait")
+            await messages.perror(ctx.channel, "Server is either being turned off or on right now. Wait")
 
 
 def setup(client):
